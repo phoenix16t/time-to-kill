3b070b21e2d9b87e484571a6ba74b771ad7185b8
Eddie Jimenez <casadebanho@gmail.com>
2015-02-11 01:43:21 -0800

    removing the app object

--- client/script.js
+++ client/script.js
@@ -1,37 +1,34 @@
 $(document).ready(function() {
-  var app;
   // TODO - validate time and location
   var venueType = $('#venueType');
   var time = $('#time');
   var location = $('#location');
   var submit = $('#submit');
+  var server = 'http://127.0.0.1:3000';
 
   ///////////////////////////////////////////////////////
   // ajax request
-  app = {
-    server: 'http://127.0.0.1:3000',
+  var killTime = function() {
 
-    killTime: function() {
-      $.ajax({
-        url: this.server,
-        type: 'POST',
-        data: JSON.stringify({
-          "venueType": venueType.val(),
-          "time": time.val(),
-          "location": location.val()
-        }),
-        contentType: 'application/json',
-        success: function(data) {
-          console.log("success", data);
-        },
-        error: function(data) {
-          console.log("error", data);
-        }
-      });
-    }
+    $.ajax({
+      url: server,
+      type: 'POST',
+      data: JSON.stringify({
+        "venueType": venueType.val(),
+        "time": time.val(),
+        "location": location.val()
+      }),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log("success", data);
+      },
+      error: function(data) {
+        console.log("error", data);
+      }
+    });
   };
 
   submit.click(function() {
-    app.killTime();
-  })
+    killTime();
+  });
 });
