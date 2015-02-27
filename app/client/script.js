c794e6a2a9f7fd7b5508f7767e561680efcc8fc3
Eddie Jimenez <casadebanho@gmail.com>
2015-02-26 23:55:43 -0800

    browserify, nodemon and geolocations

--- app/client/script.js
+++ app/client/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   // TODO - validate time and location
+  var q = require('q');
   var venueType = $('#venueType');
   var time = $('#time');
   var location = $('#location');
@@ -11,12 +12,31 @@ $(document).ready(function() {
   // mapping function
   function initialize() {
     geocoder = new google.maps.Geocoder();
-    var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+    var myLatlng = new google.maps.LatLng(-25.363882, 131.044922);
+  }
+
+  ///////////////////////////////////////////////////////
+  // get geolocation
+  function findLocation() {
+    if(navigator.geolocation) {
+      var deferred = q.defer();
+      navigator.geolocation.getCurrentPosition(function(err, result) {
+        if(err) {
+          deferred.resolve(result);
+        }
+        else {
+          deferred.resolve(result);
+        }
+      });
+      return deferred.promise;
+    }
   }
 
   ///////////////////////////////////////////////////////
   // ajax request
   var killTime = function() {
+
+    console.log("location", findLocation());
 
     geocoder.geocode({'address': location.val()}, function(results, status) {
       console.log("results", results);
--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var lint = require('gulp-jshint');
 var connect = require('gulp-connect');
 var nodemon = require('gulp-nodemon');
+var rename = require('gulp-rename');
+var browserify = require('gulp-browserify');
 
 gulp.task('lint', function() {
-  gulp.src(['app/server/*.js', 'app/client/*.js'])
+  gulp.src(['app/server/*.js', 'app/client/*.js', '!app/client/build.js'])
     .pipe(lint())
     .pipe(lint.reporter('jshint-stylish'));
 });
@@ -22,13 +24,21 @@ gulp.task('golive', function() {
     });
 });
 
+gulp.task('browserify', function() {
+  gulp.src('./app/client/script.js')
+    .pipe(browserify())
+    .pipe(rename('build.js'))
+    .pipe(gulp.dest('./app/client'));
+});
+
 gulp.task('reload', ['lint'], function() {
   gulp.src('.')
     .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
+  gulp.watch('app/client/script.js', ['browserify']);
   gulp.watch(['app/client/*.{js,html,css}'], ['reload']);
 });
 
-gulp.task('default', ['lint', 'golive', 'watch']);
+gulp.task('default', ['lint', 'golive', 'browserify', 'watch']);
